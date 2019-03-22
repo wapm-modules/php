80fa3e2d7a4af42e56493e889c5b8142987a0623
Mark McCaskey <mark@wasmer.io>
2019-03-22 10:17:26 -0700

    add changes to build cli binary; fixes for OSX

--- build.sh
+++ build.sh
@@ -12,13 +12,13 @@ rm $PHP_PATH.tar.xz
 
 echo "Apply patch"
 patch -p0 -i mods.diff
+patch -p0 -i osx_fix.diff
 
 echo "Configure"
 cd $PHP_PATH
 emconfigure ./configure \
   --disable-all \
   --disable-cgi \
-  --disable-cli \
   --disable-rpath \
   --disable-phpdbg \
   --with-valgrind=no \
@@ -30,27 +30,22 @@ emconfigure ./configure \
   --enable-json \
   --enable-ctype \
   --enable-mbstring \
-  --disable-mbregex \
-  --enable-tokenizer
+  --disable-mbregex 
 
 echo "Build"
-emmake make
+emmake make cli -j8
+# rename bitcode to something EMCC will accept
+mv sapi/cli/php sapi/cli/php.o
 mkdir -p out
-emcc -O3 -I . -I Zend -I main -I TSRM/ ../pib_eval.c -o pib_eval.o
-emcc -O3 \
-  --llvm-lto 2 \
-  -s ENVIRONMENT=web \
-  -s EXPORTED_FUNCTIONS='["_pib_eval", "_php_embed_init", "_zend_eval_string", "_php_embed_shutdown"]' \
-  -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall"]' \
-  -s MODULARIZE=1 \
-  -s EXPORT_NAME="'PHP'" \
-  -s TOTAL_MEMORY=134217728 \
-  -s ASSERTIONS=0 \
-  -s INVOKE_RUN=0 \
-  -s ERROR_ON_UNDEFINED_SYMBOLS=0 \
-  --preload-file Zend/bench.php \
-  libs/libphp7.a pib_eval.o -o out/php.html
+emcc \
+    -s EXPORTED_FUNCTIONS='["_main", "main", "WinMain"]' \
+    -s MODULARIZE=1 \
+    -s ASSERTIONS=0 \
+    -s INVOKE_RUN=0 \
+    -s ERROR_ON_UNDEFINED_SYMBOLS=0 \
+    -s TOTAL_MEMORY=134217728 \
+    sapi/cli/php.o -o php.wasm 
 
-cp out/php.wasm out/php.js out/php.data ..
+cp php.wasm .. 
 
 echo "Done"
