e1662d339efa659da335ebef5eb77272fdf15134
Syrus <me@syrusakbary.com>
2019-04-04 08:21:49 -0700

    Improved emcc generator

--- build.sh
+++ build.sh
@@ -34,18 +34,35 @@ emconfigure ./configure \
 
 echo "Build"
 emmake make cli -j8
+cp sapi/cli/php sapi/cli/php.o
 # rename bitcode file to something EMCC will accept
-mv sapi/cli/php sapi/cli/php.o
-mkdir -p out
-emcc \
-    -s EXPORTED_FUNCTIONS='["_main", "main", "WinMain"]' \
-    -s MODULARIZE=1 \
-    -s TOTAL_MEMORY=134217728 \
-    -s ASSERTIONS=0 \
-    -s INVOKE_RUN=0 \
-    -s ERROR_ON_UNDEFINED_SYMBOLS=0 \
-    -s TOTAL_MEMORY=134217728 \
-    sapi/cli/php.o -o php.wasm 
+emcc -O3 -g2 -I . -I Zend -I main -I TSRM/ ../pib_eval.c -o pib_eval.o
+emcc -O3 \
+  -g2 \
+  --llvm-lto 2 \
+  -s EXPORTED_FUNCTIONS='["_pib_eval", "_php_embed_init", "_zend_eval_string", "_php_embed_shutdown", "_main", "main"]' \
+  -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall"]' \
+  -s MODULARIZE=1 \
+  -s EXPORT_NAME="'PHP'" \
+  -s TOTAL_MEMORY=2147483648 \
+  -s ASSERTIONS=0 \
+  -s INVOKE_RUN=0 \
+  -s ERROR_ON_UNDEFINED_SYMBOLS=0 \
+  sapi/cli/php.o -o php.wasm
+
+  # -s TOTAL_MEMORY=134217728 \
+
+# mv sapi/cli/php sapi/cli/php.o
+# mkdir -p out
+# emcc \
+#     -s EXPORTED_FUNCTIONS='["_main", "main", "WinMain"]' \
+#     -s MODULARIZE=1 \
+#     -s TOTAL_MEMORY=134217728 \
+#     -s ASSERTIONS=0 \
+#     -s INVOKE_RUN=0 \
+#     -s ERROR_ON_UNDEFINED_SYMBOLS=0 \
+#     -s TOTAL_MEMORY=134217728 \
+#     sapi/cli/php.o -o php.wasm 
 
 cp php.wasm .. 
 
